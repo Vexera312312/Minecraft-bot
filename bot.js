224579913e5aa72869661a6f0e525a9e81eed12c
Vexera312312 <saasea9662@gmail.com>
2026-09-13 00:45:47 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ function createBot() {
   const bot = mineflayer.createBot({
     host: 'angelmc.play.hosting',
     port: 25565,
-    username: 'AngelmcBot',
+    username: 'Angelmc',
     version: false
   });
 
